9d55a4a93d98b90d68647766f4cd9efcecef42a9
ZehraSahaplioglu <99864727+ZehraSahaplioglu@users.noreply.github.com>
2022-12-10 22:21:58 +0300

    Update and rename HuffmanOdev.java to VeriHuffmanOdev.java

--- VeriHuffmanOdev.java
+++ VeriHuffmanOdev.java
@@ -1,15 +1,21 @@
-package huffmanodev;
+package verihuffmanodev;
 
 import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.util.Comparator;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-public class HuffmanOdev {
+public class VeriHuffmanOdev {
+
+    // secim değişkeninde bizim alfabemiz mevcut, buradakilerden farklı girdiler dikkate alınmaz.
+    static String secim = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static String hashMap;
 
     public static void printCode(HuffmanNode root, String s) {
 
         //basit durum; sol ve sağ null ise bu bir yaprak düğümdür ve ağacı çaprazlayarak oluşturulan kodları yazdırırız.
-        
         if (root.left == null && root.right == null && Character.isLetter(root.c)) {
 
             System.out.println(root.c + ":" + s);
@@ -20,42 +26,57 @@ public class HuffmanOdev {
         // sol taraf 0 kodunu alır
         // sağ taraf 1 kodunu alır
         // oluşturulan ağacın sol ve sağ alt ağacı için özyinelemeli çağırılır.
-        
         printCode(root.left, s + "0");
         printCode(root.right, s + "1");
     }
 
     public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);
+        System.out.println("Lütfen bu alfabenin dışında kalan bir harf kullanmayın: " + secim);
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("lütfen bir cumle giriniz: ");
+        String cumle = girdi.nextLine();
 
-        int n = 10;
-        char[] charArray = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k'};
-        int[] charfreq = {5, 9, 12, 13, 16, 45, 55, 60, 64, 68};
+        HashMap<Object, Object> hmap = new HashMap<Object, Object>();
+
+        for (int i = 0; i < cumle.length(); i++) {
+            if (secim.indexOf(cumle.charAt(i)) >= 0) {
+                if (!hmap.containsKey(cumle.charAt(i))) {
+                    hmap.put(cumle.charAt(i), 1);
+                } else {
+                    int count = (int) hmap.get(cumle.charAt(i));
+                    hmap.put(cumle.charAt(i), count + 1);
+                }
+            }
+
+        }
+        System.out.println(cumle); // cümleyi yazdırır
+        System.out.println(hmap); // hmap' yazdırır
+
+        Object[] a = hmap.keySet().toArray(); // dizinin anahtarlarını a dizisine atar
+        Object[] b = hmap.values().toArray(); // dizinin değerlerini b dizisine atar
+
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
 
         // öncelikli kuyruk oluşturması
-        
-        PriorityQueue<HuffmanNode> q
-                = new PriorityQueue<HuffmanNode>(n, new MyComparator());
+        PriorityQueue<HuffmanNode> q = new PriorityQueue<HuffmanNode>(cumle.length(), new MyComparator());
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < cumle.length(); i++) {
 
             // bir Huffman düğüm nesnesi oluşturmak ve onu öncelik sırasına eklemek.
-            
             HuffmanNode hn = new HuffmanNode();
 
-            hn.c = charArray[i];
-            hn.data = charfreq[i];
+            hn.c = (char) (Object) a[i];
+            hn.data = (int) (Object) b[i];
 
             hn.left = null;
             hn.right = null;
 
             // add fonksiyonu gelen veriyi huffman düğümü olarak ekler
-            
             q.add(hn);
         }
 
-        
         HuffmanNode root = null;
 
         // Burada, boyutu 1'e düşene kadar her seferinde yığından iki minimum değeri çıkaracağız, 
@@ -88,7 +109,9 @@ public class HuffmanOdev {
 
         // ağacı çaprazlayarak kodları yazdır
         printCode(root, "");
+
     }
+
 }
 
 // düğüm sınıfı, Huffman ağacında bulunan her düğümün temel yapısıdır.
@@ -103,7 +126,6 @@ class HuffmanNode {
 
 // karşılaştırıcı sınıfı, düğümün özniteliklerinden birine göre karşılaştırılmasına yardımcı olur.
 // Burada düğümlerin veri değerleri bazında karşılaştırılacağız.
-
 class MyComparator implements Comparator<HuffmanNode> {
 
     @Override
